d8bd30eed338be24c92ce22dc6bc9dd74669df66
jeffbaut13 <jeferssonabv@gmail.com>
2024-04-29 16:42:12 -0500

    add social share box

--- src/componentes/CompartirContenido.jsx
+++ src/componentes/CompartirContenido.jsx
@@ -4,6 +4,7 @@ import {
   FacebookShareButton,
   WhatsappShareButton,
 } from "react-share";
+import LoadVideo from "./LoadVideo";
 
 function CompartirContenido({ texto, url }) {
   const compartirConWebShareAPI = () => {
@@ -36,15 +37,49 @@ function CompartirContenido({ texto, url }) {
   };
 
   return (
-    <div className="flex flex-col w-2/4 text-6xl">
-      {navigator.share && (
-        <button className="my-12" onClick={compartirConWebShareAPI}>
-          Compartir
-        </button>
+    <div className="flex flex-col lg:w-2/4 sm:w-1/2 xs:w-full text-6xl flexCenter">
+      <div className="w-full rotate-180 gradiente h-screen absolute z-[2]"></div>
+      <LoadVideo
+        customStyle={"absolute top-0 left-0 z-[1]"}
+        url={"/videos/backComparte.mp4"}
+        loop={true}
+      />
+      {navigator.share ? (
+        <>
+          <button
+            className="Bebas ajusteFuente w-96 z-10"
+            onClick={compartirConWebShareAPI}
+          >
+            Compartir
+          </button>
+          <div className="flex w-96 items-center justify-between mb-16 z-10">
+            <div className="line"></div>
+            <div className=" w-4/12 p-12">
+              <img src="/svg/estrella.svg" alt="" />
+            </div>
+            <div className="line"></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <button className="Bebas ajusteFuente w-96 z-10">Compartir</button>
+          <div className="flex w-96 items-center justify-between mb-16 z-10">
+            <div className="line"></div>
+            <div className=" w-4/12 p-12">
+              <img src="/svg/estrella.svg" alt="" />
+            </div>
+            <div className="line"></div>
+          </div>
+        </>
       )}
 
-      <div className="flex justify-evenly w-full">
-        <button onClick={copiarUrl}>Copiar</button>
+      <div className="flex justify-evenly items-center w-4/6 z-10 max-lg:flex-wrap">
+        <button
+          className="btn hoverBtn max-lg:w-full max-lg:mb-16"
+          onClick={copiarUrl}
+        >
+          Copiar Link
+        </button>
         <WhatsappShareButton title={texto} url={url}>
           <span className="w-20 h-20 inline-block">
             <img src="/svg/whatapp.svg" alt="" />
