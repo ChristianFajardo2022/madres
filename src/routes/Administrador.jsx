09c82a9e8ed3a057a79bf06144c4202b57c631f6
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-02 17:44:16 -0500

    ajuste adminsitrador

--- src/routes/Administrador.jsx
+++ src/routes/Administrador.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { firestore, storage } from "../firebase/firebase-config";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { ref, getDownloadURL } from "firebase/storage";
+import axios from "axios";
 
 function Administrador() {
   const [usuarios, setUsuarios] = useState([]);
@@ -35,45 +36,67 @@ function Administrador() {
     setUsuarios(usuariosFiltrados);
   };
 
-  const handleDescargarAudio = (usuario) => {
-    if (usuario.audioURL) {
+  const descargarCSV = async () => {
+    try {
+      const response = await axios.get(
+        "https://audiosmadres.onrender.com/export-users-csv",
+        {
+          responseType: "blob",
+        }
+      );
+      const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
-      link.href = usuario.audioURL;
-      link.setAttribute("download", "audioDescargado.mp3");
+      link.href = url;
+      link.setAttribute("download", "usuarios.csv");
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
+    } catch (error) {
+      console.error("Error downloading CSV:", error);
     }
   };
 
   return (
     <div className="font-inter lowercase w-full h-screen flex flex-col justify-center items-center">
-      <div className="h-full bg-slate-400 py-4 px-4 overflow-scroll">
-        <input
-          type="text"
-          value={textoFiltro}
-          onChange={(e) => setTextoFiltro(e.target.value)}
-          className="mr-5 mb-5"
-        />
-        <select onChange={(e) => setFiltro(e.target.value)}>
-          <option value="">Seleccione un filtro</option>
-          <option value="email">Email</option>
-          <option value="firstname">Nombre</option>
-        </select>
-        <button onClick={filtrarUsuarios}>Filtrar</button>
-        <ul className="mr-5 mb-5 h-full">
+      <button className="btn active mb-12" onClick={descargarCSV}>
+        descargar DB
+      </button>
+      <div className="min-h-96 bg-zinc-700 py-4 px-4 flexCenter flex-wrap rounded-3xl">
+        <div className="w-full flex justify-evenly">
+          <input
+            type="text"
+            value={textoFiltro}
+            onChange={(e) => setTextoFiltro(e.target.value)}
+            className="rounded-full px-4"
+          />
+          <select
+            className="rounded-full px-4"
+            onChange={(e) => setFiltro(e.target.value)}
+          >
+            <option value="">Seleccione un filtro</option>
+            <option value="email">Email</option>
+            <option value="firstname">Nombre</option>
+          </select>
+          <button
+            className="hover:text-black hover:bg-white transition text-white border border-white px-4 py-2 rounded-full"
+            onClick={filtrarUsuarios}
+          >
+            Filtrar
+          </button>
+        </div>
+        <ul className="mr-5 my-5 h-full text-white">
           {usuarios.map((usuario) => (
-            <li key={usuario.id} className="mr-5">
-              {usuario.firstname} - {usuario.email}
-              <button onClick={() => handleDescargarAudio(usuario)}>
-                Descargar
-              </button>
+            <>
+              <li key={usuario.id} className="w-full my-6">
+                <span className="text-xl font-black">{usuario.firstname}</span>{" "}
+                - {usuario.email}
+              </li>
               <audio
                 id={`audioElement_${usuario.id}`}
                 controls
-                src={usuario.audioURL}
+                src={`${usuario.audioURL}.mp3`}
               ></audio>
-            </li>
+            </>
           ))}
         </ul>
       </div>
