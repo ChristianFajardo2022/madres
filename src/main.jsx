76e92704769ed14b972d77435bdae173e15a4471
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-02 16:56:55 -0500

    google analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import "./index.css";
 import "./mediaqueries.css";
 import ReactGA4 from "react-ga4";
 
-/* ReactGA4.initialize("G-73DSGQ4GWK");
+ReactGA4.initialize("G-LKH5WPP3C0");
 ReactGA4.send({
   hitType: "pageview",
   page: window.location.pathname,
-}); */
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/routes/Administrador.jsx
+++ src/routes/Administrador.jsx
@@ -1,74 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { firestore, storage } from '../firebase/firebase-config';
-import { collection, query, where, getDocs } from 'firebase/firestore';
-import { ref, getDownloadURL } from 'firebase/storage';
+import React, { useState, useEffect } from "react";
+import { firestore, storage } from "../firebase/firebase-config";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { ref, getDownloadURL } from "firebase/storage";
 
 function Administrador() {
-    const [usuarios, setUsuarios] = useState([]);
-    const [filtro, setFiltro] = useState('');
-    const [textoFiltro, setTextoFiltro] = useState('');
+  const [usuarios, setUsuarios] = useState([]);
+  const [filtro, setFiltro] = useState("");
+  const [textoFiltro, setTextoFiltro] = useState("");
 
-    useEffect(() => {
-        const obtenerUsuarios = async () => {
-            const q = query(collection(firestore, "usuarios"));
-            const querySnapshot = await getDocs(q);
-            let usuarios = [];
-            querySnapshot.forEach((doc) => {
-                usuarios.push({ id: doc.id, ...doc.data() });
-            });
-            setUsuarios(usuarios);
-        };
+  const filtrarUsuarios = async () => {
+    if (!textoFiltro) {
+      setUsuarios([]);
+      return;
+    }
 
-        obtenerUsuarios();
-    }, []);
-
-    const filtrarUsuarios = async () => {
-        if (!textoFiltro) return;
-
-        const q = query(collection(firestore, "usuarios"), where(filtro, "==", textoFiltro));
-        const querySnapshot = await getDocs(q);
-        let usuariosFiltrados = [];
-        querySnapshot.forEach((doc) => {
-            usuariosFiltrados.push({ id: doc.id, ...doc.data() });
-        });
-        setUsuarios(usuariosFiltrados);
-    };
-
-    const descargarAudio = async (audioRef) => {
-        try {
-            const audioURL = await getDownloadURL(ref(storage, audioRef));
-            const link = document.createElement('a');
-            link.href = audioURL;
-            link.setAttribute('download', 'audioDescargado.mp3');  // Asegúrate de que la extensión corresponda al tipo de archivo
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        } catch (error) {
-            console.error("Error al obtener la URL de descarga: ", error);
-        }
-    };    
-    return (
-        <div className='w-full h-screen flex flex-col justify-center items-center'>
-            <div className=' bg-slate-400 py-4 px-4'>
-            <input type="text" value={textoFiltro} onChange={(e) => setTextoFiltro(e.target.value)} className='mr-5 mb-5' />
-            <select onChange={(e) => setFiltro(e.target.value)}>
-                <option value="nombre">Nombre</option>
-                <option value="cedula">Cédula</option>
-                <option value="correoElectronico">Correo Electrónico</option>
-                <option value="numeroWhatsapp">Número de WhatsApp</option>
-            </select>
-            <button onClick={filtrarUsuarios}>Filtrar</button>
-            <ul className='mr-5 mb-5'>
-                {usuarios.map(usuario => (
-                    <li key={usuario.id} className='mr-5'>
-                        {usuario.nombre} - {usuario.cedula} - {usuario.correoElectronico}
-                        <button target-blank onClick={() => descargarAudio(usuario.audioRef)} className='mr-5 mb-5 ml-5 bg-red-500 rounded-lg px-4'>Descargar Audio</button>
-                    </li>
-                ))}
-            </ul>
-            </div>
-        </div>
+    const q = query(
+      collection(firestore, "usuarios"),
+      where(filtro, "==", textoFiltro)
     );
+    const querySnapshot = await getDocs(q);
+    const usuariosFiltrados = await Promise.all(
+      querySnapshot.docs.map(async (doc) => {
+        const data = doc.data();
+        try {
+          const url = await getDownloadURL(ref(storage, data.audioRef));
+          return { id: doc.id, ...data, audioURL: url };
+        } catch (error) {
+          console.error("Error al obtener URL de descarga", error);
+          return { id: doc.id, ...data, audioURL: "" }; // Si falla la descarga, proporciona una URL vacía.
+        }
+      })
+    );
+
+    setUsuarios(usuariosFiltrados);
+  };
+
+  const handleDescargarAudio = (usuario) => {
+    if (usuario.audioURL) {
+      const link = document.createElement("a");
+      link.href = usuario.audioURL;
+      link.setAttribute("download", "audioDescargado.mp3");
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
+  return (
+    <div className="font-inter lowercase w-full h-screen flex flex-col justify-center items-center">
+      <div className="h-full bg-slate-400 py-4 px-4 overflow-scroll">
+        <input
+          type="text"
+          value={textoFiltro}
+          onChange={(e) => setTextoFiltro(e.target.value)}
+          className="mr-5 mb-5"
+        />
+        <select onChange={(e) => setFiltro(e.target.value)}>
+          <option value="">Seleccione un filtro</option>
+          <option value="email">Email</option>
+          <option value="firstname">Nombre</option>
+        </select>
+        <button onClick={filtrarUsuarios}>Filtrar</button>
+        <ul className="mr-5 mb-5 h-full">
+          {usuarios.map((usuario) => (
+            <li key={usuario.id} className="mr-5">
+              {usuario.firstname} - {usuario.email}
+              <button onClick={() => handleDescargarAudio(usuario)}>
+                Descargar
+              </button>
+              <audio
+                id={`audioElement_${usuario.id}`}
+                controls
+                src={usuario.audioURL}
+              ></audio>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Administrador;
