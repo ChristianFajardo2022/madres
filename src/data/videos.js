eea50b7786d5b88b36435622d92ce6d0d6587072
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-03 17:52:47 -0500

    script tag manager

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,6 +1,6 @@
 export const videos = {
   comercial: "https://www.youtube.com/watch?v=YBGU3g6Tbrk",
-  makinOff: "https://www.youtube.com/watch?v=YBGU3g6Tbrk",
+  makinOff: "https://www.youtube.com/watch?v=B1PYiOkP85k",
   caboRamirez: "https://www.youtube.com/watch?v=YBGU3g6Tbrk",
   sargento: "https://www.youtube.com/watch?v=YBGU3g6Tbrk",
 };
