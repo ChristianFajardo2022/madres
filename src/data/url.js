2a703299216c73d671e161e52e6c0b4ab5786be4
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-03 09:29:38 -0500

    test alcarrito

--- src/data/url.js
+++ src/data/url.js
@@ -1,5 +1,4 @@
 export const url = "https://audiosmadres.onrender.com";
 export const urlDev = "http://localhost:3000";
 export const urlAlcarrito = "https://www.alcarrito.com";
-export const urlAlcarritoDev =
-  "https://mcstaging.alcarrito.com/operacionmayo/comprar";
+export const urlAlcarritoDev = "https://www.mercadolibre.com.co/";
