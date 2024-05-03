c7cd6c9d6b3a7552b7b08e49b05aaf0c77aa6572
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-03 16:03:25 -0500

    cambio url produccion

--- src/data/url.js
+++ src/data/url.js
@@ -1,6 +1,5 @@
 export const urlServer = "https://audiosmadres.onrender.com";
 //export const urlServer = "http://localhost:3000";
 
-//export const urlAlcarrito = "https://www.alcarrito.com";
-export const urlAlcarrito =
-  "https://mcstaging.alcarrito.com/operacionmayo/comprar";
+export const urlAlcarrito = "https://alcarrito.com/operacionmayo/comprar";
+//export const urlAlcarrito = "https://mcstaging.alcarrito.com/operacionmayo/comprar";
