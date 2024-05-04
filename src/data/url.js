74076a1088ef2d73d710733646fa95aa464166fa
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-04 17:07:09 -0500

    ajuste url a produccion

--- src/data/url.js
+++ src/data/url.js
@@ -1,7 +1,5 @@
 export const urlServer = "https://audiosmadres.onrender.com";
 //export const urlServer = "http://localhost:3000";  url servidor
 
-//export const urlAlcarrito = "https://www.alcarrito.com/operacionmayo/addtocart";
-export const urlAlcarrito =
-  "https://mcstaging.alcarrito.com/operacionmayo/addtocart";
-//export const urlAlcarrito = "https://www.mercadolibre.com.co";
+export const urlAlcarrito = "https://www.alcarrito.com/operacionmayo/addtocart";
+//export const urlAlcarrito = "https://mcstaging.alcarrito.com/operacionmayo/addtocart";
