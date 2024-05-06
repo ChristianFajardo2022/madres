899bde901412cd6767bc666a7b9d3bbe27af3713
jeffbaut13 <jeferssonabv@gmail.com>
2024-05-06 10:28:06 -0500

    Cambio video makin of

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,6 +1,6 @@
 export const videos = {
   comercial: "https://www.youtube.com/watch?v=ZlyOPxjvXt8",
-  makinOff: "https://www.youtube.com/watch?v=B1PYiOkP85k",
+  makinOff: "https://www.youtube.com/watch?v=ZlyOPxjvXt8",
   caboRamirez: "https://www.youtube.com/shorts/_f6wHU-wpZg",
   sargento: "https://www.youtube.com/shorts/TH2mvNWmjec",
 };
