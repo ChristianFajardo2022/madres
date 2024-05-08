370eb478235f96a3ad1c9da7e03eca1757fe8f0b
jefersson bautista <98704711+jeffbaut13@users.noreply.github.com>
2024-05-08 07:54:51 -0500

    Update url.js

--- src/data/url.js
+++ src/data/url.js
@@ -3,5 +3,4 @@ export const urlServer = "https://audiosmadres.onrender.com";
 //export const urlServer = "https://audiosmadrestest.onrender.com";  url servidor
 
 export const urlAlcarrito = "https://www.alcarrito.com/operacionmayo/addtocart";
-//export const urlAlcarrito =
-  "https://mcstaging.alcarrito.com/operacionmayo/addtocart";
+//export const urlAlcarrito = "https://mcstaging.alcarrito.com/operacionmayo/addtocart";
